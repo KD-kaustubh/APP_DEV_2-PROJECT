4471aef09fa5c15659ab1f9a7aeaa3fa827af1a1
KD-kaustubh <ram.shivaay100@gmail.com>
2026-01-03 23:34:13 +0530

    updated

--- application/celery_schedule.py
+++ application/celery_schedule.py
@@ -8,11 +8,11 @@ def setup_periodic_tasks():
     celery_app.conf.beat_schedule = {
         'daily-reminder': {
             'task': 'daily_reminder',
-            'schedule': crontab(hour=10, minute=50),
+            'schedule': crontab(hour=20, minute=8),
         },
         'monthly-report': {
             'task': 'monthly_report',
-            'schedule': crontab( hour=10, minute=56),
+            'schedule': crontab(hour=20, minute=8),
         },
     }
     celery_app.conf.timezone = 'Asia/Kolkata'
